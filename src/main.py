8ced3c6f9aa0e5c1d5d068fdb4dfde5a7427b033
dunnwithpuns <dunnwithpuns@gmail.com>
2024-04-23 13:45:25 -0500

    first working base

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from textnode import TextNode
 
 def main():
-    TextNode("This is a text node", "bold", "https://www.boot.dev")
+    print(TextNode("This is a text node", "bold", "https://www.boot.dev"))
 
 if __name__ == "__main__":
     main()
