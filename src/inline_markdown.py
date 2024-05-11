de6753c2146174e42488217ed4a9cd4b31aca227
dunnwithpuns <dunnwithpuns@gmail.com>
2024-05-11 14:43:42 -0500

    finished split_nodes_image and split_nodes_link functions and tests

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -7,35 +7,6 @@ from textnode import (
     text_type_link,
 )
 
-def process_split_nodes_image(old_node, images):     
-    split_nodes = [] 
-    text = old_node.text
-    for image in images: 
-        sections = text.split(f"![{image[0]}]({image[1]})", 1)
-        if len(sections) != 2:
-            raise ValueError("Invalid markdown, image section not close")
-        if sections[0] != "":
-            split_nodes.append(TextNode(sections[0], text_type_text))
-        split_nodes.append(TextNode(image[0], text_type_image, image[1]))
-        text = sections[1]
-    if text.strip():
-        split_nodes.append(TextNode(text, text_type_text))
-
-    return split_nodes
-
-# split image nodes
-def split_nodes_image(old_nodes):
-    new_nodes = []
-    for old_node in old_nodes:
-        if old_node.text_type != text_type_text:
-           new_nodes.append(old_node)
-           continue
-        images = extract_md_images(old_node.text)
-        if not images:
-            return old_node   
-        new_nodes.extend(process_split_nodes_image(old_node, images))
-    return new_nodes 
-
 # function processing split nodes 
 def process_split_nodes_delimiter(old_node, delimiter, text_type):
     sections = old_node.text.split(delimiter) 
@@ -52,10 +23,8 @@ def process_split_nodes_delimiter(old_node, delimiter, text_type):
     
     return split_nodes
 
-# splitting nodes based on their delimiter
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
-
     for old_node in old_nodes:
         if old_node.text_type != text_type_text:
             new_nodes.append(old_node)
@@ -63,10 +32,68 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         new_nodes.extend(process_split_nodes_delimiter(old_node, delimiter, text_type))
     return new_nodes 
 
+def process_split_nodes_link(old_node, links):
+    split_nodes = []
+    text = old_node.text
+    for link in links:
+        sections = text.split(f"[{link[0]}]({link[1]})", 1) 
+        if sections[0] != "":
+            split_nodes.append(TextNode(sections[0], text_type_text))
+        split_nodes.append(TextNode(link[0], text_type_link, link[1]))
+        text = sections[1]
+    if text.strip():
+        split_nodes.append(TextNode(text, text_type_text))
+    return split_nodes
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != text_type_text:
+            new_nodes.append(old_node)
+            continue
+        links = extract_md_links(old_node.text)
+        if not links:
+           new_nodes.append(old_node)
+           continue
+        new_nodes.extend(process_split_nodes_link(old_node, links))
+    return new_nodes
+
+def process_split_nodes_image(old_node, images):     
+    split_nodes = [] 
+    text = old_node.text
+    for image in images: 
+        sections = text.split(f"![{image[0]}]({image[1]})", 1) 
+        if sections[0] != "":
+            split_nodes.append(TextNode(sections[0], text_type_text))
+        split_nodes.append(TextNode(image[0], text_type_image, image[1]))
+        text = sections[1]
+    if text.strip():
+        split_nodes.append(TextNode(text, text_type_text))
+    return split_nodes
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != text_type_text:
+           new_nodes.append(old_node)
+           continue
+        images = extract_md_images(old_node.text)
+        if not images:
+            new_nodes.append(old_node)
+            continue   
+        new_nodes.extend(process_split_nodes_image(old_node, images))
+    return new_nodes 
+
 def extract_md_images(text):
-    matches = re.findall(r"!\[(.*?)\]\((.*?)\)", text)
+    pattern = r"!\[(.*?)\]\((.*?)\)"
+    matches = re.findall(pattern, text)
+    if not matches:
+        raise ValueError("Inavlid markdown, image section not closed")
     return matches
 
 def extract_md_links(text):
-    matches = re.findall(r"\[(.*?)\]\((.*?)\)", text)
+    pattern = r"\[(.*?)\]\((.*?)\)"
+    matches = re.findall(pattern, text)
+    if not matches:
+        raise ValueError("Invalid markdown, link section not closed")
     return matches
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -3,7 +3,8 @@ from inline_markdown import (
     split_nodes_delimiter,
     extract_md_images,
     extract_md_links,
-    split_nodes_image
+    split_nodes_image,
+    split_nodes_link,
 )
 
 from textnode import (
@@ -12,7 +13,8 @@ from textnode import (
     text_type_bold,
     text_type_italic,
     text_type_code,
-    text_type_image
+    text_type_image,
+    text_type_link,
 )
 
 
@@ -42,11 +44,26 @@ class testInlineMarkdown(unittest.TestCase):
         text = "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and ![another](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png)"
         expected = [("image", "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png"), ("another", "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png")]
         self.assertEqual(extract_md_images(text), expected)
+
+    def test_extract_md_images_err(self):
+        text = "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png cool!"
+        with self.assertRaises(ValueError):
+            extract_md_images(text)
     
+    def test_extract_md_images_err1(self):
+        text = "This is text with an [image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) cool!"
+        with self.assertRaises(ValueError):
+            extract_md_images(text)
+
     def test_extract_md_links(self):
         text = "This is text with a [link](https://www.example.com) and [another](https://www.example.com/another)"
         expected = [("link", "https://www.example.com"), ("another", "https://www.example.com/another")]
         self.assertEqual(extract_md_links(text), expected)
+    
+    def test_extract_md_links_err(self):
+        text = "This is text with a [link](https://www.example.com "
+        with self.assertRaises(ValueError):
+            extract_md_links(text)
 
     def test_split_nodes_image_single(self):
         node = TextNode("This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) cool!", text_type_text)
@@ -66,6 +83,49 @@ class testInlineMarkdown(unittest.TestCase):
             TextNode("second image", text_type_image, "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png"),
             ] 
         self.assertEqual(split_nodes_image([node]), expected)
+    
+    def test_split_nodes_image_err(self):
+        node = TextNode("This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png cool!", text_type_text)
+        with self.assertRaises(ValueError):
+            split_nodes_image([node])
+
+    def test_split_nodes_link_single(self):
+        node = TextNode("This is text with a [link](https://www.google.com) cool!", text_type_text)
+        expected = [
+            TextNode("This is text with a ", text_type_text),
+            TextNode("link", text_type_link, "https://www.google.com"),
+            TextNode(" cool!", text_type_text),
+        ]
+        self.assertEqual(split_nodes_link([node]), expected)
+
+    def test_split_nodes_link_double(self):
+        node = TextNode("This is text with a [link](https://www.google.com) and another [second link](https://www.boot.dev)", text_type_text)
+        expected = [
+            TextNode("This is text with a ", text_type_text),
+            TextNode("link", text_type_link, "https://www.google.com"),
+            TextNode(" and another ", text_type_text),
+            TextNode("second link", text_type_link, "https://www.boot.dev"),
+        ]
+        self.assertEqual(split_nodes_link([node]), expected)
+    
+    def test_split_nodes_link_err(self):
+        node = TextNode("This is text with a [link](https://www.google.com", text_type_text)
+        with self.assertRaises(ValueError):
+            split_nodes_link([node])
+
+    def test_split_nodes_link_multiple(self):
+        node1 = TextNode("This is text with a [link](https://www.google.com) cool!", text_type_text)
+        node2 = TextNode("This is text with a [link](https://www.google.com) and another [second link](https://www.boot.dev) cool!", text_type_text)
+        node_list = [node1, node2]
+        expected = [
+            TextNode("This is text with a ", text_type_text),
+            TextNode("link", text_type_link, "https://www.google.com"),
+            TextNode(" cool!", text_type_text),
+            TextNode("This is text with a ", text_type_text),
+            TextNode("link", text_type_link, "https://www.google.com"), 
+            TextNode(" and another ", text_type_text),
+            TextNode("second link", text_type_link, "https://www.boot.dev") 
+        ]
 
 if __name__ == "__main__":
     unittest.main() 
