10795eeb8db1dec160f056acb11aa8de0526e5c5
dunnwithpuns <dunnwithpuns@gmail.com>
2024-06-18 09:33:44 -0500

    updating __to_html methods

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -3,6 +3,8 @@ from htmlnode import(
     ParentNode,
     LeafNode,
 )
+from inline_markdown import text_to_textnodes
+from textnode import text_node_to_html_node
 
 block_type_paragraph = "paragraph"
 block_type_heading = "heading"
@@ -26,6 +28,7 @@ def markdown_to_html_node(markdown):
     nodes = []
     for block in blocks:
         html_node = block_to_html_node(block)
+        nodes.append(html_node)
     return ParentNode("div", nodes)
 
 def block_to_html_node(block):
@@ -41,6 +44,45 @@ def block_to_html_node(block):
     if block_type == block_type_ordered_list:
         return ordered_to_html(block)
     raise ValueError("Invalid block type")
+
+def block_to_block_type(block):
+    lines = block.split("\n")
+
+    if block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return block_type_paragraph 
+        return block_type_quote
+    if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        return block_type_heading
+    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
+        return block_type_code   
+    if block.startswith("1. "): 
+        i = 1
+        for line in lines:
+            if not line.startswith("{i}. "):
+                return block_type_paragraph
+            i += 1
+        return block_type_ordered_list 
+    if block.startswith("* "):
+        for line in lines:
+            if not line.startswith("* "):
+                return block_type_paragraph
+        return block_type_unordered_list 
+    if block.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return block_type_paragraph
+        return block_type_unordered_list 
+    return block_type_paragraph
+
+def text_to_children(text):
+    text_nodes = text_to_textnodes(text)
+    children = []
+    for text_node in text_nodes:
+        html_node = text_node_to_html_node(text_node)
+        children.append(html_node)
+    return children
 
 def paragraph_to_html(block, type):
     if type != block_type_paragraph:
@@ -68,23 +110,15 @@ def ordered_to_html(block, type):
     return ParentNode("li", LeafNode("ol", block))
 
 def heading_to_html(block, type=block_type_heading):
-    return 
+    level = 0
+    for char in block:
+        if char == "#":
+            level += 1
+        else:
+            break
+    if level + 1 >= len(block):
+        raise ValueError(f"Invalide heading level: {level}")
+    text = block[level + 1 :]
+    children = text_to_children(text)
+    return ParentNode(f"h{level}", children)
 
-def block_to_block_type(block):
-    if block.startswith(">"):
-        return block_type_quote
-    if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
-        return block_type_heading
-    if block.startswith("```") and block.endswith("```"):
-        return block_type_code   
-    if block.startswith("1. "):
-        lines = block.split("\n") 
-        i = 1
-        for line in lines:
-            if not line.startswith("{i}. "):
-                return block_type_ordered_list
-            i += 1
-    elif block.startswith("* ") or block.startswith("- "):
-        return block_type_unordered_list
-    else:
-        return block_type_paragraph
