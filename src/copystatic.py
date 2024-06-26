2b00854a46c7001921efba8c497de0e2a7a97dd3
dunnwithpuns <dunnwithpuns@gmail.com>
2024-06-25 20:04:40 -0500

    created copystatic.py which recursively copies all files in the static dir into the public dir

---
+++ src/copystatic.py
@@ -0,0 +1,17 @@
+import os
+import shutil
+
+def copy_files(source_dir, destination_dir):
+    
+    if not os.path.exists(destination_dir):
+        os.mkdir(destination_dir) 
+    
+    for item in os.listdir(source_dir):
+        from_path = os.path.join(source_dir, item)
+        to_path = os.path.join(destination_dir, item)
+        print(f" * {from_path} -> {to_path}")
+        if os.path.isfile(from_path):
+            shutil.copy(from_path, to_path)
+        else:
+            copy_files(from_path, to_path)
+
--- src/main.py
+++ src/main.py
@@ -1,10 +1,18 @@
-from textnode import TextNode
+import os
+import shutil
 
-from htmlnode import HTMLNode, LeafNode, ParentNode
+from copystatic import copy_files
+
+dir_path_static = "./static"
+dir_path_public = "./public"
 
 def main():
-    print(TextNode("This is an image link", "image", "https://www.google.com"))
+    print(f"Deleting public directory. . .")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
 
+    print(f"Copying files to public directory. . .")
+    copy_files(dir_path_static, dir_path_public)
 
 if __name__ == "__main__":
     main()
