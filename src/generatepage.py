0e200a02995586324a228e01cd12a0ce1799f57a
dunnwithpuns <dunnwithpuns@gmail.com>
2024-07-01 14:41:43 -0500

    working out generate pages recursive function

--- src/generatepage.py
+++ src/generatepage.py
@@ -31,7 +31,13 @@ def generate_page(from_path, template_path, dest_path):
         f.write(template)
 
 def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
-    if os.path.isfile(dir_path_content):
-        generate_page(dir_path_content, template_path, dest_dir_path)
-    else:
-        pass
+    dirs = []
+    if not os.path.isfile(dir_path_content): 
+        dirs = os.listdir(dir_path_content)
+
+    for d in dirs:
+        if os.path.isfile(d):
+            generate_page(d, template_path, dest_dir_path)
+            continue
+        
+        generate_pages_recursive(os.path.join(dir_path_content, d), template_path, os.path.join(dest_dir_path, d))
--- src/main.py
+++ src/main.py
@@ -2,13 +2,13 @@ import os
 import shutil
 
 from copystatic import copy_files
-from generatepage import generate_page
+from generatepage import generate_pages_recursive, generate_page
 
 dir_path_static = "./static"
 dir_path_public = "./public"
 template_path = "./template.html"
 html_path = "./public/index.html"
-markdown_path = "./content/index.md"
+markdown_path = "./content/majesty/index.html"
 
 def main():
     print(f"Deleting public directory. . .")
@@ -18,7 +18,7 @@ def main():
     print(f"Copying files to public directory. . .")
     copy_files(dir_path_static, dir_path_public)
  
-    generate_page(markdown_path, template_path, html_path)
+    generate_page(markdown_path, template_path, html_path) 
 
 if __name__ == "__main__":
     main()
